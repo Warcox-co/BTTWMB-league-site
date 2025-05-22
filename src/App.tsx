fcc6fae10c0d93a98d1bfe6105a1e4e3d8939769
Warcox-co <122842743+Warcox-co@users.noreply.github.com>
2025-05-21 22:21:23 -0400

    Starting site

---
+++ src/App.tsx
@@ -0,0 +1,38 @@
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'
+import Teams from './components/Teams'
+import leagueHeader from './assets/theLeagueHeader.avif'
+import './App.css'
+
+function Home() {
+  return (
+    <div className="league-home">
+      <main>
+        {/* League content will go here */}
+      </main>
+    </div>
+  )
+}
+
+function App() {
+  return (
+    <Router>
+      <div className="app-container">
+        <header className="site-header">
+          <img src={leagueHeader} alt="League Header" className="league-header-image" />
+        </header>
+        
+        <nav className="main-nav">
+          <Link to="/">Home</Link>
+          <Link to="/teams">Teams</Link>
+        </nav>
+
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/teams" element={<Teams />} />
+        </Routes>
+      </div>
+    </Router>
+  )
+}
+
+export default App
---
+++ src/components/Teams.tsx
@@ -0,0 +1,14 @@
+function Teams() {
+  return (
+    <div className="teams-page">
+      <header>
+        <h1>Teams</h1>
+      </header>
+      <main>
+        {/* Team list will go here */}
+      </main>
+    </div>
+  )
+}
+
+export default Teams
